bf700222ab27a395585f49d8525700713dddf607
Matthias Wessendorf <matzew@apache.org>
2012-08-22 13:15:20 +0200

    Basic implementation of AGPipeline, and added some tests + kiwi specs

--- AeroGear-iOS/AeroGear-iOSTests/AGHttpClientTests.m
+++ AeroGear-iOS/AeroGear-iOSTests/AGHttpClientTests.m
@@ -1,10 +1,19 @@
-//
-//  AGHttpClientTests.m
-//  AeroGear-iOS
-//
-//  Created by matzew on 21.08.12.
-//  Copyright (c) 2012 JBoss. All rights reserved.
-//
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "AGHttpClientTests.h"
 #import "AGHttpClient.h"
@@ -31,7 +40,7 @@
 // =================================================
 // CREATE section
 // =================================================
--(void) testPostProject {
+-(void) xtestPostProject {
     // the object, fairly simple...just as a data structure... no object mapping yet..
     NSMutableDictionary* parameters = [NSMutableDictionary dictionary];
     
@@ -65,7 +74,7 @@
 // READ section
 // =================================================
 
--(void) testGetProjects {
+-(void) xtestGetProjects {
     
     [restClient getPath:@"projects" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
         
