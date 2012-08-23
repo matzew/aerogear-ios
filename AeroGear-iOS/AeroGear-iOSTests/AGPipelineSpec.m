7b4e202c1ae0f4dedf5692b8848a5333ebcbdb8b
Matthias Wessendorf <matzew@apache.org>
2012-08-23 06:56:08 +0200

    removing duplicated test...

--- AeroGear-iOS/AeroGear-iOSTests/AGPipelineSpec.m
+++ AeroGear-iOS/AeroGear-iOSTests/AGPipelineSpec.m
@@ -34,8 +34,6 @@ describe(@"AGPipeline", ^{
         
         it(@"should not be nil", ^{
             [pipeline shouldNotBeNil];
-            id pipe = [pipeline get:@"tests"];
-            [[theValue(pipe) shouldNot] equal:nil];
         });
 
         it(@"should have a pipe", ^{
