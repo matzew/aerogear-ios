80fd0f24bd13ec3cc657723264fcf01375afb934
Matthias Wessendorf <matzew@apache.org>
2012-08-24 11:31:59 +0200

    Adding new test case, to show the entire (current) API; Running against RHCLOUD.....

---
+++ AeroGear-iOS/AeroGear-iOSTests/AGPipelineUsecaseTests.h
@@ -0,0 +1,13 @@
+//
+//  AGPipelineUsecaseTests.h
+//  AeroGear-iOS
+//
+//  Created by matzew on 24.08.12.
+//  Copyright (c) 2012 JBoss. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AGPipelineUsecaseTests : SenTestCase
+
+@end
