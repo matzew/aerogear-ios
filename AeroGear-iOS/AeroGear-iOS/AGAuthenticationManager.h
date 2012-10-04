e6017f44f8fc0f3c32a542c9c2d7ade931f1239f
Matthias Wessendorf <matzew@apache.org>
2012-10-04 15:57:26 +0200

    Adding inital API for login,logout,register and a management API

---
+++ AeroGear-iOS/AeroGear-iOS/AGAuthenticationManager.h
@@ -0,0 +1,70 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+#import "AGAuthenticationModule.h"
+
+/**
+ * AGAuthenticationManager manages different AGAuthenticationModule implementations. It is basically a
+ * factory that hides the concrete instanciation of a specific AGAuthenticationModule implementation.
+ * The class offers simple APIs to add, remove or get access to a 'authentication module'.
+ *
+ */
+@interface AGAuthenticationManager : NSObject
+
+
+/**
+ * A factory method to instantiate an empty AGAuthenticationManager.
+ *
+ * @return the AGAuthenticationManager object
+ */
++(id) manager;
+
+/**
+ * Creates a new default (REST) AGAuthenticationModule implemention.
+ *
+ * @param moduleName The name of the actual auth module object.
+ * @param baseURL the base url for the server.
+ */
+-(id<AGAuthenticationModule>)add:(NSString*) moduleName baseURL:(NSURL*)baseURL;
+
+/**
+ * Creates a new AGAuthenticationModule implemention. The actual type is determined by the type argument.
+ *
+ * @param moduleName The name of the actual auth module object.
+ * @param baseURL the base url for the server.
+ * @param type The type of the new auth module object.
+ */
+-(id<AGAuthenticationModule>)add:(NSString*) moduleName baseURL:(NSURL*)baseURL type:(NSString*) type;
+
+/**
+ * Removes a AGAuthenticationModule implemention from the AGAuthenticationManager. The auth module,
+ * to be removed is determined by the moduleName argument.
+ *
+ * @param moduleName The name of the actual auth module object.
+ */
+-(id<AGAuthenticationModule>)remove:(NSString*) moduleName;
+
+/**
+ * Loads a given AGAuthenticationModule implemention, based on the given moduleName argument.
+ *
+ * @param moduleName The name of the actual auth module object.
+ */
+-(id<AGAuthenticationModule>)get:(NSString*) moduleName;
+
+
+@end
---
+++ AeroGear-iOS/AeroGear-iOS/AGAuthenticationModule.h
@@ -0,0 +1,78 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+
+/**
+ * AGAuthenticationModule represents an authentication module...
+ */
+@protocol AGAuthenticationModule <NSObject>
+
+/**
+ * Performs a signup of a new user. The request accepts a NSDictionary which will be translated to JSON 
+ * and send to the endpoint.
+ *
+ * @param userData A map (NSDictionary) containing all the information requested by the
+ * 'registration' service.
+ *
+ * @param success A block object to be executed when the operation finishes successfully.
+ * This block has no return value and takes one argument: A collection (NSDictionary), containing the response
+ * from the 'signup' service.
+ *
+ * @param failure A block object to be executed when the operation finishes unsuccessfully.
+ * This block has no return value and takes one argument: The `NSError` object describing
+ * the error that occurred.
+ */
+-(id) signup:(id) userData
+     success:(void (^)(id object))success
+     failure:(void (^)(NSError *error))failure;
+
+/**
+ * Performs the login of the give user. Since the data will be send in plaintext, it is IMPORTANT,
+ * to run the signin via TLS/HTTPS.
+ *
+ * @param username username
+ *
+ * @param password password
+ *
+ * @param success A block object to be executed when the operation finishes successfully.
+ * This block has no return value and takes one argument: A collection (NSDictionary), containing the response
+ * from the 'login' service.
+ *
+ * @param failure A block object to be executed when the operation finishes unsuccessfully.
+ * This block has no return value and takes one argument: The `NSError` object describing
+ * the error that occurred.
+ */
+-(id) login:(NSString*) username
+    password:(NSString*) password
+     success:(void (^)(id object))success
+     failure:(void (^)(NSError *error))failure;
+
+/**
+ * Performs the logout of the current user.
+ *
+ * @param success A block object to be executed when the operation finishes successfully.
+ * This block has no return value and takes no argument.
+ *
+ * @param failure A block object to be executed when the operation finishes unsuccessfully.
+ * This block has no return value and takes one argument: The `NSError` object describing
+ * the error that occurred.
+ */
+-(id) logout:(void (^)())success
+     failure:(void (^)(NSError *error))failure;
+
+@end
