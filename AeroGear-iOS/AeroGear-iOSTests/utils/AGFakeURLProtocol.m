9d94eba7b11b8d7d6bb48b164985227957c2fc46
Christos Vasilakis <cvasilak@gmail.com>
2012-11-28 09:47:18 +0200

    fake http comm. using an override of NSURLProtocol

---
+++ AeroGear-iOS/AeroGear-iOSTests/utils/AGFakeURLProtocol.m
@@ -0,0 +1,89 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "AGFakeURLProtocol.h"
+
+@interface NSHTTPURLResponse(UndocumentedInitializer)
+- (id)initWithURL:(NSURL*)URL statusCode:(NSInteger)statusCode headerFields:(NSDictionary*)headerFields requestTime:(double)requestTime;
+@end
+
+static NSData* sResponseData;
+static NSDictionary* sHeaders;
+static NSInteger sStatusCode = 200;
+static NSError* sError;
+static NSString* sMethod;
+
+@implementation AGFakeURLProtocol
+
++ (BOOL)canInitWithRequest:(NSURLRequest*)request {
+	return [[[request URL] scheme] isEqualToString:@"http"];
+}
+
++ (NSURLRequest*)canonicalRequestForRequest:(NSURLRequest*)request {
+	return request;
+}
+
++ (void)setResponseData:(NSData*)data {
+    sResponseData = data;
+}
+
++ (void)setHeaders:(NSDictionary*)headers {
+    sHeaders = headers;
+}
+
++ (void)setStatusCode:(NSInteger)statusCode {
+    sStatusCode = statusCode;
+}
+
++ (void)setError:(NSError*)error {
+    sError = error;
+}
+
+- (NSCachedURLResponse*)cachedResponse {
+	return nil;
+}
+
++ (NSString*)methodCalled {
+    return sMethod;
+}
+
+- (void)startLoading {
+    NSURLRequest *request = [self request];
+    
+    sMethod = [request HTTPMethod];
+
+	id<NSURLProtocolClient> client = [self client];
+	
+	if(sResponseData) {
+		NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc] initWithURL:[request URL]
+																  statusCode:sStatusCode
+																headerFields:sHeaders
+																 requestTime:0.0];
+		
+		[client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+		[client URLProtocol:self didLoadData:sResponseData];
+		[client URLProtocolDidFinishLoading:self];
+	} else if(sError) {
+		[client URLProtocol:self didFailWithError:sError];
+	}
+}
+
+- (void)stopLoading {
+}
+
+@end
