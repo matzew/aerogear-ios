c5ca8893bddd02ed842caf8f7a50e7ca2f272424
Matthias Wessendorf <matzew@apache.org>
2013-01-21 18:55:46 +0100

    Updating test to verify basic parsing for WebLink and Body...

--- AeroGear-iOS/AeroGear-iOSTests/AGPaginationRawTest.m
+++ AeroGear-iOS/AeroGear-iOSTests/AGPaginationRawTest.m
@@ -32,7 +32,8 @@
 
 -(void)setUp {
     [super setUp];
-    _baseURL = [NSURL URLWithString:@"http://localhost:8080/aerogear-controller-demo/"];
+    //_baseURL = [NSURL URLWithString:@"http://localhost:8080/aerogear-controller-demo/"];
+    _baseURL = [NSURL URLWithString:@"https://api.github.com/users/matzew/"];
     
     _restClient = [AGHttpClient clientFor:_baseURL];
     _restClient.parameterEncoding = AFJSONParameterEncoding;
@@ -45,39 +46,84 @@
     [super tearDown];
 }
 
+-(void) testReceivingLinkHeader {
 
-/**
- * A static shared "pagigng context"; it stores the "AG-...-NEXT" header.
- *
- * It is static, to make sure it is valid between the different test() executions
- */
-NSDictionary *pagingState;
-
--(void) testPagingFiveCars {
-    
-    // start scrolling......
-    pagingState = @{@"offset" : @"1", @"limit" : @"5"};
-    
-    [_restClient getPath:@"cars" parameters:pagingState success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"\n\n%@\n", responseObject);
-        
-        
-        
-        /// genereate the 'next' information.... MOVE to function...., arg is the header.......
-        NSString *next = [[[operation response] allHeaderFields] valueForKey:@"AG-Links-Next"];
-        // get rid of the resource?
-        NSRange range = [next rangeOfString:@"?"];
-        next = [next substringFromIndex:range.location+1];
-        // chop the query into a dictionary
-        NSArray *components = [next componentsSeparatedByString:@"&"];
-        NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
-        for (NSString *component in components) {
-            [parameters setObject:[[component componentsSeparatedByString:@"="] objectAtIndex:1] forKey:[[component componentsSeparatedByString:@"="] objectAtIndex:0]];
+    [_restClient getPath:@"gists" parameters:@{@"page" : @"2", @"per_page" : @"1"} success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSMutableDictionary *pagingLinks = [NSMutableDictionary dictionary];
+        NSArray *links = [[[[operation response] allHeaderFields] valueForKey:@"Link"] componentsSeparatedByString:@","];
+        for (NSString *link in links) {
+            NSArray *elementsPerLink = [link componentsSeparatedByString:@";"];
+            
+            NSURL *parsedURL;
+            for (NSString *elem in elementsPerLink) {
+                // funny TRIN
+                NSString *tmpElem = [elem stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+                // URL...
+                if ([tmpElem hasPrefix:@"<"] && [tmpElem hasSuffix:@">"]) {
+                    
+                    // TODO.... rel. url... (could use extractQueryArgs)
+                    
+                    parsedURL = [NSURL URLWithString:[[tmpElem substringFromIndex:1] substringToIndex:tmpElem.length-2]]; //2 because, the first did already cut one char...
+                } else if ([tmpElem hasPrefix:@"rel="]) { // only rel...
+                    NSString *rel = [[tmpElem substringFromIndex:5] substringToIndex:tmpElem.length-6]; // cutting 5 + the last....
+                    [pagingLinks setValue:parsedURL forKey:rel];
+                } else {
+                    // ignore title etc
+                }
+            }
         }
+     
+        NSLog(@"THE links map: \n\n%@\n", pagingLinks);
+     
+        // TODO... add a category to NSURL that returns the query string as a dictonary.... we ONLY
+        // need that.....
+     
+     
+        _finishedFlag = YES;
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+
+    // keep the run loop going
+    while(!_finishedFlag) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+    }
+}
+
+// TO BE USED for HEAD and BODY strat.
+-(NSDictionary *) extractQueryArgs:(NSString *) value {
+    
+    NSRange range = [value rangeOfString:@"?"];
+    value = [value substringFromIndex:range.location+1];
+    // chop the query into a dictionary
+    NSArray *components = [value componentsSeparatedByString:@"&"];
+    NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+    for (NSString *component in components) {
+        [parameters setObject:[[component componentsSeparatedByString:@"="] objectAtIndex:1] forKey:[[component componentsSeparatedByString:@"="] objectAtIndex:0]];
+    }
+    return parameters;
+}
+
+-(void) testTwitterBits {
+    
+    _baseURL = [NSURL URLWithString:@"http://search.twitter.com/"];
+    _restClient = [AGHttpClient clientFor:_baseURL];
+    _restClient.parameterEncoding = AFJSONParameterEncoding;
+    
+    [_restClient getPath:@"search.json" parameters:@{@"q" : @"aerogear", @"page" : @"2", @"rpp" : @"5"} success:^(AFHTTPRequestOperation *operation, id responseObject) {
         
-        // stash the result, to reference it for the 'next' invoke
-        pagingState = parameters;
+        // TODO, read the KEY from config........
+        NSString *nextIdentifier = @"next_page";
+        NSString *prevIdentifier = @"previous_page";
         
+        // buld the MAP of links....:
+        NSMutableDictionary *mapOfLink = [NSMutableDictionary dictionary];
+        
+        [mapOfLink setValue:[self extractQueryArgs:[responseObject valueForKey:nextIdentifier]] forKey:nextIdentifier];
+        [mapOfLink setValue:[self extractQueryArgs:[responseObject valueForKey:prevIdentifier]] forKey:prevIdentifier];
+        
+        
+        NSLog(@"THE links map: \n\n%@\n", mapOfLink);
         
         _finishedFlag = YES;
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
@@ -88,48 +134,96 @@ NSDictionary *pagingState;
     while(!_finishedFlag) {
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
     }
+
+    
 }
 
--(void) testPagingFiveCarsAndNext {
-    
-    // now, ..... HERE is the 'next()' invoke...
-    
-    [_restClient getPath:@"cars" parameters:pagingState success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"\n\n%@\n", responseObject);
-        _finishedFlag = YES;
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"Error: %@", error);
-    }];
-    
-    // keep the run loop going
-    while(!_finishedFlag) {
-        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
-    }
-}
 
--(void)testReadWithFilter {
-    AGPipeline *testPipeline = [AGPipeline pipelineWithBaseURL:_baseURL];
-    id<AGPipe> pipe = [testPipeline pipe:^(id<AGPipeConfig> config) {
-        [config setName:@"cars"];
-    }];
-    
-    
-    [pipe readWithFilter:^(id<AGFilterConfig> config) {
-        // hrm...
-//        [config setLimit:3];
-//        [config setOffset:0];
-        
-    } success:^(id responseObject) {
-        NSLog(@"\n\n -- > %@", [responseObject description]);
-        _finishedFlag = YES;
-    } failure:^(NSError *error) {
-    }];
-    
-    
-    // keep the run loop going
-    while(!_finishedFlag) {
-        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
-    }
-}
+
+///**
+// * A static shared "pagigng context"; it stores the "AG-...-NEXT" header.
+// *
+// * It is static, to make sure it is valid between the different test() executions
+// */
+//NSDictionary *pagingState;
+//
+//-(void) testPagingFiveCars {
+//    
+//    // start scrolling......
+//    pagingState = @{@"offset" : @"1", @"limit" : @"5"};
+//    
+//    [_restClient getPath:@"cars" parameters:pagingState success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        NSLog(@"\n\n%@\n", responseObject);
+//        
+//        
+//        
+//        /// genereate the 'next' information.... MOVE to function...., arg is the header.......
+//        NSString *next = [[[operation response] allHeaderFields] valueForKey:@"AG-Links-Next"];
+//        // get rid of the resource?
+//        NSRange range = [next rangeOfString:@"?"];
+//        next = [next substringFromIndex:range.location+1];
+//        // chop the query into a dictionary
+//        NSArray *components = [next componentsSeparatedByString:@"&"];
+//        NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+//        for (NSString *component in components) {
+//            [parameters setObject:[[component componentsSeparatedByString:@"="] objectAtIndex:1] forKey:[[component componentsSeparatedByString:@"="] objectAtIndex:0]];
+//        }
+//        
+//        // stash the result, to reference it for the 'next' invoke
+//        pagingState = parameters;
+//
+//        
+//        _finishedFlag = YES;
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        NSLog(@"Error: %@", error);
+//    }];
+//    
+//    // keep the run loop going
+//    while(!_finishedFlag) {
+//        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+//    }
+//}
+//
+//-(void) testPagingFiveCarsAndNext {
+//    
+//    // now, ..... HERE is the 'next()' invoke...
+//    
+//    [_restClient getPath:@"cars" parameters:pagingState success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        NSLog(@"\n\n%@\n", responseObject);
+//        _finishedFlag = YES;
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        NSLog(@"Error: %@", error);
+//    }];
+//    
+//    // keep the run loop going
+//    while(!_finishedFlag) {
+//        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+//    }
+//}
+//
+//-(void)testReadWithFilter {
+//    AGPipeline *testPipeline = [AGPipeline pipelineWithBaseURL:_baseURL];
+//    id<AGPipe> pipe = [testPipeline pipe:^(id<AGPipeConfig> config) {
+//        [config setName:@"cars"];
+//    }];
+//    
+//    
+//    [pipe readWithFilter:^(id<AGFilterConfig> config) {
+//        // hrm...
+////        [config setLimit:3];
+////        [config setOffset:0];
+//        
+//    } success:^(id responseObject) {
+//        NSLog(@"\n\n -- > %@", [responseObject description]);
+//        _finishedFlag = YES;
+//    } failure:^(NSError *error) {
+//    }];
+//    
+//    
+//    // keep the run loop going
+//    while(!_finishedFlag) {
+//        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+//    }
+//}
 
 @end
