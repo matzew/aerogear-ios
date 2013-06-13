5d3fc6ac96d0587ab8399fb7e452c162f0b0722b
Matthias Wessendorf <matzew@apache.org>
2013-06-13 10:53:15 +0200

    clean up

--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -workspace AeroGear-iOS/AeroGear-iOS.xcworkspace/ -scheme AeroGear-iOS && xctool -workspace AeroGear-iOS/AeroGear-iOS.xcworkspace/ -scheme AeroGear-iOSTests && xctool -workspace AeroGear-iOS/AeroGear-iOS.xcworkspace -scheme AeroGear-iOS test
+xctool -workspace AeroGear-iOS/AeroGear-iOS.xcworkspace/ -scheme AeroGear-iOS && xctool -workspace AeroGear-iOS/AeroGear-iOS.xcworkspace/ -scheme AeroGear-iOSTests
 
