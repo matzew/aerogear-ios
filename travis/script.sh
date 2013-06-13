d0784d5540fb0bc4323cbf8b182aba06a799701d
Matthias Wessendorf <matzew@apache.org>
2013-06-13 10:39:54 +0200

    Moving travis hook into script

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -workspace AeroGear-iOS/AeroGear-iOS.xcworkspace/ -scheme AeroGear-iOS && xctool -workspace AeroGear-iOS/AeroGear-iOS.xcworkspace/ -scheme AeroGear-iOSTests
+xctool -workspace AeroGear-iOS/AeroGear-iOS.xcworkspace/ -scheme AeroGear-iOS && xctool -workspace AeroGear-iOS/AeroGear-iOS.xcworkspace/ -scheme AeroGear-iOSTests && xctool -workspace AeroGear-iOSTests.xcworkspace -scheme AeroGear-iOS test
+
