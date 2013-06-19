9f8454098c7c05ec599adaacf5888be89bbbaea3
Christos Vasilakis <cvasilak@gmail.com>
2013-06-19 10:58:56 +0300

    upgrade afnetworking lib to 1.3.1

--- AeroGear-iOS/Pods/Pods-environment.h
+++ AeroGear-iOS/Pods/Pods-environment.h
@@ -8,8 +8,7 @@
 
 // AFNetworking
 #define COCOAPODS_POD_AVAILABLE_AFNetworking
-// This library does not follow semantic-versioning,
-// so we were not able to define version macros.
-// Please contact the author.
-// Version: 1.0RC1.
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
