51e62fab8fa216fae542f03c5b100b90df088d79
Christos Vasilakis <cvasilak@gmail.com>
2013-10-03 11:18:31 +0300

    added missing api doc

--- AeroGear-iOS/AeroGear-iOS/datamanager/AGPropertyListStorage.m
+++ AeroGear-iOS/AeroGear-iOS/datamanager/AGPropertyListStorage.m
@@ -18,14 +18,41 @@
 #import "AGPropertyListStorage.h"
 
 /**
-  Provides a common interface around NSPropertyListSerialization and NSJSONSerialization
-  plist output formats.
+ * Provides a common interface around NSPropertyListSerialization and NSJSONSerialization
+ * plist output formats. See AGPListEncoder class for plist encoding and AGJsonEncoder class for JSON
+ * encoding.
  */
 @protocol AGEncoder <NSObject>
 
+/**
+ * Returns an NSData object containing a given property list encoded in the respective 
+ * serialization format.
+ *
+ * @param plist A valid property list object to be encoded.
+ * @param error An error object containing details of why the encode failed.
+ *
+ * @return An NSData object containing plist encoded in the respective serialization format.
+ */
 -(NSData *) encode:(id)plist error:(NSError **)error;
+
+/**
+ * Creates and returns a property list from the specified data.
+ *
+ * @param data A collection (e.g. NSArray) which is being persisted.
+ * @param error An error object containing details of why the decode failed.
+ *
+ * @return A property list object corresponding to the representation in data. If data is 
+ *         not in a supported format, returns nil.
+ */
 -(id)       decode:(NSData *)data error:(NSError **)error;
 
+/**
+ * Returns a Boolean value that indicates whether a given property list is valid for a given serialization format.
+ *
+ * @param plist A property list object.
+ *
+ * @return YES if plist is a valid property list in format format, otherwise NO.
+ */
 -(BOOL) isValid:(id)plist;
 
 @end
